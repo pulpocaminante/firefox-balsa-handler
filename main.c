f8ead8c9123831ea7d47c3a59bdb3c3a7a180709
pulpocaminante <109849915+pulpocaminante@users.noreply.github.com>
2024-06-15 10:27:46 -0700

    UL

--- main.c
+++ main.c
@@ -5,14 +5,31 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-	if (argc != 2)
-	{
-		printf("Usage: %s <mailto>\n");
-		return 1;
-	}
-	if (execlp("balsa", "balsa", "-m", argv[1], NULL) == -1) {
-	fprintf(stderr, "Error executing balsa: %s\n", strerror(errno));
-		return 1;
-	}
-	return 0;	
+    if (argc < 2 || argc > 4) {
+        printf("Usage: %s <mailto> [subject] [body]\n", argv[0]);
+        return 1;
+    }
+
+    char *mailto = argv[1];
+    char *subject = argc > 2 ? argv[2] : NULL;
+    char *body = argc > 3 ? argv[3] : NULL;
+
+    if (subject && body) {
+        if (execlp("balsa", "balsa", "-m", mailto, "-s", subject, "-b", body, NULL) == -1) {
+            fprintf(stderr, "Error executing balsa: %s\n", strerror(errno));
+            return 1;
+        }
+    } else if (subject) {
+		 if (execlp("balsa", "balsa", "-m", mailto, "-s", subject, NULL) == -1) {
+            fprintf(stderr, "Error executing balsa: %s\n", strerror(errno));
+            return 1;
+    }
+    } else {
+        if (execlp("balsa", "balsa", "-m", mailto, NULL) == -1) {
+            fprintf(stderr, "Error executing balsa: %s\n", strerror(errno));
+            return 1;
+        }
+    }
+
+    return 0;
 }
